775a9d1d8fa6c82f9776762ec38ac3852f7cc847
Albert Cervera i Areny <albert@nan-tic.com>
2013-12-31 19:40:59 +0100

    Initial commit.

---
+++ doc/index.rst
@@ -0,0 +1,6 @@
+Project Invoice Hours Module
+############################
+
+The Project Invoice Hours module adds a new invoice method on projects named
+On Hours for which the invoices are created based on the hours in field *Hours*
+for all children works in state *Done*.
---
+++ work.py
@@ -0,0 +1,71 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+from decimal import Decimal
+
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+
+__all__ = ['Work']
+__metaclass__ = PoolMeta
+
+
+class Work:
+    __name__ = 'project.work'
+
+    @classmethod
+    def __setup__(cls):
+        super(Work, cls).__setup__()
+
+        # Add hours to project_invoice_method
+        item = ('hours', 'On Hours')
+        if item not in cls.project_invoice_method.selection:
+            cls.project_invoice_method.selection.append(item)
+
+    @staticmethod
+    def _get_invoiced_hours_hours(works):
+        Work = Pool().get('project.work')
+        # Needed to browse invoice lines
+        with Transaction().set_user(0, set_context=True):
+            works = Work.browse([x.id for x in works])
+        return dict((w.id, w.invoice_line.quantity or 0.0) for w in works
+            if w.invoice_line)
+
+    @staticmethod
+    def _get_hours_to_invoice_hours(works):
+        return dict((w.id, w.hours or 0.0) for w in works
+            if w.state == 'done' and not w.invoice_line)
+
+    @staticmethod
+    def _get_invoiced_amount_hours(works):
+        pool = Pool()
+        InvoiceLine = pool.get('account.invoice.line')
+
+        with Transaction().set_user(0, set_context=True):
+            invoice_lines = InvoiceLine.browse([
+                    w.invoice_line.id for w in works
+                    if w.invoice_line])
+
+        id2invoice_lines = dict((l.id, l) for l in invoice_lines)
+        amounts = {}
+        for work in works:
+            if work.invoice_line:
+                invoice_line = id2invoice_lines[work.invoice_line.id]
+                amounts[work.id] = invoice_line.amount
+            else:
+                amounts[work.id] = Decimal(0)
+        return amounts
+
+    def _get_lines_to_invoice_hours(self):
+        if not self.invoice_line and self.hours and self.state == 'done':
+            if not self.product:
+                self.raise_user_error('missing_product', (self.rec_name,))
+            elif self.list_price is None:
+                self.raise_user_error('missing_list_price', (self.rec_name,))
+            return [{
+                    'product': self.product,
+                    'quantity': self.hours,
+                    'unit_price': self.list_price,
+                    'origin': self,
+                    'description': self.work.name,
+                    }]
+        return []
