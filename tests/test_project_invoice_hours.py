c1ff23fa8eee1173b66ab7c15e805bf3690c80f6
?ngel ?lvarez <angel@nan-tic.com>
2018-02-01 14:39:47 +0100

    fix test and rename locales

--- tests/test_project_invoice_hours.py
+++ tests/test_project_invoice_hours.py
@@ -5,7 +5,8 @@ import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class ProjectInvoiceHoursTestCase(ModuleTestCase):
@@ -17,7 +18,9 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             ProjectInvoiceHoursTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_project_invoice_hours.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_project_invoice_hours.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
