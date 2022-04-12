1fbcf4a24b425274e38fb63438992e6d2d178df2
Àngel Àlvarez <angel@nan-tic.com>
2022-04-12 19:55:00 +0200

    copy _get_quantiy_to_invoice_timesheet to round to 5 minuntes before convert to hours

--- work.py
+++ work.py
@@ -2,7 +2,17 @@
 # this repository contains the full copyright notices and license terms.
 from decimal import Decimal
 from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+from collections import defaultdict
+import datetime
+import math
+from sql import Null
+from sql.aggregate import Sum
+from trytond.tools import reduce_ids, grouped_slice
 
+def round_time(seconds, round_minutes=5):
+    round_seconds = round_minutes * 60
+    return math.ceil(seconds / round_seconds) * round_seconds
 
 class Work(metaclass=PoolMeta):
     __name__ = 'project.work'
@@ -21,7 +31,7 @@ class Work(metaclass=PoolMeta):
         quantities = cls._get_quantity_to_invoice_timesheet(works)
         for work_id, hours in quantities.items():
             work = cls(work_id)
-            if (work.status and work.status.progress != 1) or work.invoice_line:
+            if work.invoice_line:
                 quantities[work_id] = 0.0
         return quantities
 
@@ -54,3 +64,48 @@ class Work(metaclass=PoolMeta):
         if self.invoice_method == 'hours':
             origins.append(self)
         return origins
+
+    @classmethod
+    def _get_quantity_to_invoice_timesheet(cls, works):
+        pool = Pool()
+        TimesheetLine = pool.get('timesheet.line')
+        cursor = Transaction().connection.cursor()
+        line = TimesheetLine.__table__()
+
+        upto2tworks = defaultdict(list)
+        twork2work = {}
+        for work in works:
+            upto = work.invoice_timesheet_up_to
+            for timesheet_work in work.timesheet_works:
+                twork2work[timesheet_work.id] = work.id
+                upto2tworks[upto].append(timesheet_work.id)
+
+        durations = defaultdict(datetime.timedelta)
+        query = line.select(
+            line.work, Sum(line.duration),
+            group_by=line.work)
+        for upto, tworks in upto2tworks.items():
+            for sub_ids in grouped_slice(tworks):
+                query.where = (reduce_ids(line.work, sub_ids)
+                    & (line.invoice_line == Null))
+                if upto:
+                    query.where &= (line.date <= upto)
+                cursor.execute(*query)
+
+                for twork_id, duration in cursor:
+                    if duration:
+                        # SQLite uses float for SUM
+                        if not isinstance(duration, datetime.timedelta):
+                            duration = datetime.timedelta(seconds=duration)
+                        durations[twork2work[twork_id]] += duration
+
+        quantities = {}
+        for work in works:
+            duration = durations[work.id]
+            if work.list_price:
+                duration = round_time(duration.total_seconds())
+                hours = duration / 60 / 60
+                if work.unit_to_invoice:
+                    hours = work.unit_to_invoice.round(hours)
+                quantities[work.id] = hours
+        return quantities
